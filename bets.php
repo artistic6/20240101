9b5555c1eead99060e9e54c93300e999981c8bba
artistic6 <123533552+artistic6@users.noreply.github.com>
2024-03-05 17:01:15 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -84,13 +84,12 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
             else $index = "f$horse-f$favorite";
             if(isset($threes[$raceNumber][$index])){
                     $threeSet = $threes[$raceNumber][$index];
-                    $racetext .= "\t\t'$index' => '" . $threeSet . "',\n";
                     $unionCF = array_values(array_unique(array_merge($unionCF, explode(", ", $threeSet))));
                 }
     }
     if(!empty($unionCF)){
         sort($unionCF);
-        $racetext .= "\t\t'union current favorite' => '" . implode(", ", $unionCF) . "',\n";
+        $racetext .= "\t\t'union CF' => '" . implode(", ", $unionCF) . "',\n";
     }
     $racetext .= "\t],\n";
     unset($oldFavorites);
