aeb5e7e58bfd4a46728876d773e8a4fb3ff11ce6
artistic6 <artisticsoftware6@gmail.com>
2024-03-05 05:50:42 +0000

    ...

---
+++ 0306/bets.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Favorite'  =>  '10',
+		'favorites' => '10',
+		'union' => '1, 4, 5, 8, 10, 11',
+		'union current favorite' => '1, 4, 5, 8, 10, 11',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Favorite'  =>  '4',
+		'favorites' => '4',
+		'union' => '4, 5, 7, 12',
+		'union current favorite' => '4, 5, 7, 12',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Favorite'  =>  '5',
+		'favorites' => '5',
+		'union' => '5, 8, 10',
+		'union current favorite' => '5, 8, 10',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Favorite'  =>  '5',
+		'favorites' => '5',
+		'union' => '4, 10, 12',
+		'union current favorite' => '4, 10, 12',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Favorite'  =>  '3',
+		'favorites' => '3',
+		'union' => '1, 3, 4, 7, 12',
+		'union current favorite' => '1, 3, 4, 7, 12',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Favorite'  =>  '1',
+		'favorites' => '1',
+		'union' => '1, 3, 7, 9, 12',
+		'union current favorite' => '1, 3, 7, 9, 12',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Favorite'  =>  '9',
+		'favorites' => '9',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Favorite'  =>  '3',
+		'favorites' => '3',
+		'union' => '5, 6, 7, 12',
+		'union current favorite' => '5, 6, 7, 12',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'Favorite'  =>  '11',
+		'favorites' => '11',
+	],
+];
