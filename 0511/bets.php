6737dd39c7f281a804c96ef2f916a42fb6c80af8
artistic6 <artisticsoftware6@gmail.com>
2024-05-10 04:34:20 +0000

    ...

---
+++ 0511/bets.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '13',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '3',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '1',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '6',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '6',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '12',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '2',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '3',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '8',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '10',
+	],
+];
