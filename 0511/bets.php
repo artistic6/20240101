d4f7a8b452bd487d804fe18196e9962cc0de299e
artistic6 <artisticsoftware6@gmail.com>
2024-05-10 08:03:23 +0000

    ...

--- 0511/bets.php
+++ 0511/bets.php
@@ -12,22 +12,22 @@ return [
 		Race 2
 		*/
 		'favorites' => '3, 5',
+		'union' => '5',
+		'inter' => '5',//count: 1
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '1',
-		'potential inter(fav 3)' => '1, 3',
-		'union' => '1, 3',
-		'inter' => '1',
-		'history(1)' => '1, 3, 4, 9, 11',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '6, 8',
+		'union' => '10, 11',
+		'inter' => '',//count: 0
 	],
 	'5' => [
 		/**
@@ -40,33 +40,30 @@ return [
 		Race 6
 		*/
 		'favorites' => '1, 3, 12',
+		'union' => '1, 2, 3, 4',
+		'inter' => '1, 3',//count: 2
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '2, 7',
-		'potential inter(fav 5)' => '2, 7',
 		'union' => '2, 7',
-		'inter' => '2, 7',
-		'history(2)' => '2, 5, 7, 9',
-		'history(7)' => '2, 6, 7, 8, 11',
+		'inter' => '2, 7',//count: 2
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'favorites' => '3',
-		'potential inter(fav 8)' => '3, 8',
-		'union' => '3, 8',
-		'inter' => '3',
-		'history(3)' => '3, 6, 7, 8',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '4, 8',
+		'union' => '5, 12',
+		'inter' => '',//count: 0
 	],
 	'10' => [
 		/**
