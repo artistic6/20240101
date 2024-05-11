a95e6f51a4f1d1bc7e57d8a922c9b96f82c827b5
artistic6 <artisticsoftware6@gmail.com>
2024-05-11 07:56:35 +0000

    ...

--- 0511/bets.php
+++ 0511/bets.php
@@ -20,15 +20,15 @@ return [
 		Race 3
 		*/
 		'favorites' => '1, 5',
-		'union' => '3, 4',
-		'inter' => '',//count: 0
+		'union' => '3, 4, 5',
+		'inter' => '5',//count: 1
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '1, 6, 8',
-		'union' => '1, 4, 5, 10, 11',
+		'union' => '1, 2, 4, 5, 10, 11',
 		'inter' => '1',//count: 1
 	],
 	'5' => [
@@ -36,15 +36,15 @@ return [
 		Race 5
 		*/
 		'favorites' => '3, 6',
-		'union' => '1, 7, 10, 11',
-		'inter' => '',//count: 0
+		'union' => '1, 3, 7, 10, 11',
+		'inter' => '3',//count: 1
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '1, 3, 12',
-		'union' => '1, 2, 3, 4',
+		'union' => '1, 2, 3, 4, 9',
 		'inter' => '1, 3',//count: 2
 	],
 	'7' => [
